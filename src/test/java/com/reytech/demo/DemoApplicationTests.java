bde8ea52e6bb6c8e57b921aaf4a8e5e9b6b22597
1dyhia <131856630+1dyhia@users.noreply.github.com>
2023-07-03 11:39:59 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -3,8 +3,18 @@ package com.reytech.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
+	@Test
+	void groupAssertions(){
+	Personne pers = new Personne("Nassur","Mouhamadi");
+	assertAll("peronne",()-> assertEquals("Nassur", pers.getFirstName()),
+	() -> assertEquals("Mouhamadi",pers.getLastName()));
+	}
+	
 
 	@Test
 	void contextLoads() {
